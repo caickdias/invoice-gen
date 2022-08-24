d865b750b7d77f0907718e16344a9cd174774969
caickdias <caickandrade0@gmail.com>
2022-08-24 17:44:32 -0300

    added too many arguments error

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const { createPdf, addCompany } = require('./utils/functions.js');
 
 const SHOW_COMPANIES = 'showCompanies';
 const ADD_COMPANY = 'addCompany';
+const CREATE_PDF_DEFAULT = 'createPdfDefault';
+const CREATE_PDF_COMPANY = 'createPdfCompany';
+const TOO_MANY_ARGUMENTS = 'tooManyArguments';
 
 const handleArgs = (arg) => {
-  const command = getCommand(arg.join());
+  const command = getCommand(arg);
+  let defaultId = 0;
   
   switch(command){
     case SHOW_COMPANIES:
@@ -16,11 +20,25 @@ const handleArgs = (arg) => {
     case ADD_COMPANY:
       addCompany();
       break;
+    case CREATE_PDF_DEFAULT:      
+      createPdf({
+        ...info,
+        company: companies[defaultId],
+      });
+      break;
+    case CREATE_PDF_COMPANY:
+      console.log('id');
+      break;
+    case TOO_MANY_ARGUMENTS:
+      console.log('Too many arguments');
+      break;
   }
 
 }
 
 const getCommand = (command) => {
+  if(command.length > 1) return TOO_MANY_ARGUMENTS;
+  if(command == '') return CREATE_PDF_DEFAULT;
   if(command == '-c' || command == '--companies') return SHOW_COMPANIES;
   if(command == '-a' || command == '--add-company') return ADD_COMPANY;
 }
